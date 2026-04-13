19473d26d4b7526d4aa405c4c3c352a3b804e046
savageme6ix <maingithomas803@gmail.com>
2026-04-13 21:58:30 +0300

    Styled images, setting up queries

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,7 +8,22 @@ const Home = () => {
                 <span className="text-[red]">Hand</span>crafted in small batches using the world's <span className="text-[gold]">finest</span> botanical oils. Welcome to luxury you can actually wear"
             </h1>
 
-            <img src="/blur.jpg"/>
+            <img src="/sunDrawing.jpg"
+              alt="image"
+              className="shadow-xl hover:scale-105 transition-transform duration-300 w-[300px] absolute top-10 left-20 rotate-[-12deg] z-0 opacity-100 rounded-2xl max-lg:rotate-[2deg] left-[15px]"
+            />
+            <img src="/happy.jpg"
+              alt="image"
+              className="shadow-xl animate-in fade-in zoom-in duration-500 hover:scale-105 transition-transform duration-300 w-[300px] absolute bottom-20 right-10 rotate-[8deg] z-20 opacity-100 rounded-2xl max-lg:z-0 rotate-[12deg]"
+            />
+            <img src="/blur.jpg"
+              alt="image"
+              className="shadow-xl animate-in fade-in zoom-in duration-500 hover:scale-105 transition-transform duration-300 w-[300px] absolute bottom-80 right-10 rotate-[0deg] z-0 opacity-100 rounded-2xl max-lg:opacity-70"
+            />
+            <img src="/heart.jpg"
+              alt="image"
+              className="shadow-xl animate-in fade-in zoom-in duration-500 hover:scale-105 transition-transform duration-300 w-[300px] absolute top-1/2 left-1/3 rotate-[-25deg] z-0 opacity-40 rounded-2xl max-lg:opacity-0"
+            />
         </div>
     </main>
   )
